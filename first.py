fced163a91623bc343a16555eced1a80a6ddd479
Pranshul Lakhanpal <pranshul@Pranshuls-MacBook-Pro.local>
2020-07-07 20:35:13 -0700

    finished first chapter, kind of learned the importance of a .json file.

--- first.py
+++ first.py
@@ -1,3 +1,5 @@
+from tweepy import Stream
+from tweepy.streaming import StreamListener
 import tweepy
 from tweepy import OAuthHandler
 import json
@@ -9,7 +11,6 @@ access_secret = 'HlOuor0ujivGsFP4SuHrVeasOQp4kfK2SWTkJWs8PTAzv'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
-
 api = tweepy.API(auth)
 
 
@@ -27,9 +28,28 @@ for status in tweepy.Cursor(api.home_timeline).items(10):
 
 #prints the list of followers
 
-for friend in tweepy.Cursor(api.friends).items():
-    process_or_store(friend.screen_name)
+# for friend in tweepy.Cursor(api.friends).items():
+#     process_or_store(friend.screen_name)
 
 
-for tweet in tweepy.Cursor(api.user_timeline).items():
-    process_or_store(tweet.text)
+# for tweet in tweepy.Cursor(api.user_timeline).items():
+    # process_or_store(tweet.text)
+
+class MyListener(StreamListener):
+
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#python'])
