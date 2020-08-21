24a9c7e4fb01b5bc36ca2a6d03a075f791833620
Pranshul Lakhanpal <44278407+PranshulLol@users.noreply.github.com>
2020-08-21 12:03:43 -0700

    tested tokanising

--- second.py
+++ second.py
@@ -1,7 +1,10 @@
 import json
 from nltk.tokenize import word_tokenize 
+# nltk.download('punkt')
+# with open('python.json', 'r') as f:
+#     line = f.readline()
+#     tweet = json.loads(line)
+#     print(json.dumps(tweet, indent=4))
+tweet = 'RT @marcobonzanini: just an example! :D http://example.com #NLP'
+print(word_tokenize(tweet))
 
-with open('python.json', 'r') as f:
-    line = f.readline()
-    tweet = json.loads(line)
-    print(json.dumps(tweet, indent=4))
